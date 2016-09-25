4996a39702fe8c171007eae4d73be7ad740596cd
dimaloga <dianiloga@gmail.com>
2016-09-25 15:09:33 -0500

    Update main.js
    ..

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,29 +7,43 @@
  * # MainCtrl
  * Controller of the heyIamHereApp
  */
+//angular.module('heyIamHereApp', ['heyIamHereAppService']);
+
 angular.module('heyIamHereApp').controller('MainCtrl', function ($scope, $rootScope) {
-    console.log('Ahora si te tenemos en la mira');
+
+  console.log("Entra al controlador");
 
   $rootScope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyBaU4xLICEvs3_uxtqpa0zC7YhgQWh9jL0";
 
      $rootScope.logLatLng = function() {
           console.log("logLatLng");
     };
-
-    $rootScope.wayPoints = [
-          {location: {lat:44.32384807250689, lng: -78.079833984375}, stopover: true},
-          {location: {lat:44.55916341529184, lng: -76.17919921875}, stopover: true},
-     ];
     
     $rootScope.wayPointOrigin = [
           44.32384807250689, -78.079833984375
     ];
 
     $rootScope.wayPointDestination = [
-          44.55916341529184, 
-          -76.17919921875
+          44.55916341529184, -76.17919921875
     ];
 
-    console.log('Termina el controlador');
+    $scope.posts={};
 
+    /*$scope.getAllPosts = function(){
+      testRequest.posts().success(function (data){
+        $scope.posts=data; // Asignaremos los datos de todos los posts
+        $scope.posts.exist=1;
+      });
+    }
+
+    $scope.getPost = function(){  
+      $scope.unPost={};
+      testRequest.post($scope.post_id).success(function (data){
+        $scope.unPost=data; // Asignaremos los datos del post
+        $scope.unPost.exist=1;
+        $scope.posts.exist=0;
+      });
+    }*/
+
+    console.log("Sale del controlador");
   });
