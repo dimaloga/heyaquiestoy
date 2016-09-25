c85ace619eed0ce743d71c5e836aa7486d8ec9ef
pablopaho <papatino@sura.com.co>
2016-09-24 22:02:08 -0500

    Conexion con maps punto a punto

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,16 +10,24 @@
 angular.module('heyIamHereApp').controller('MainCtrl', function ($scope, $rootScope) {
     console.log('Ahora si te tenemos en la mira');
 
-  $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyBaU4xLICEvs3_uxtqpa0zC7YhgQWh9jL0";
+  $rootScope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyBaU4xLICEvs3_uxtqpa0zC7YhgQWh9jL0";
 
-     $scope.logLatLng = function(e) {
+     $rootScope.logLatLng = function() {
           console.log("logLatLng");
-          console.log('loc', e.latLng);
-    }
-    
-    $scope.wayPoints = [
+    };
+
+    $rootScope.wayPoints = [
           {location: {lat:44.32384807250689, lng: -78.079833984375}, stopover: true},
           {location: {lat:44.55916341529184, lng: -76.17919921875}, stopover: true},
+     ];
+    
+    $rootScope.wayPointOrigin = [
+          44.32384807250689, -78.079833984375
+    ];
+
+    $rootScope.wayPointDestination = [
+          44.55916341529184, 
+          -76.17919921875
     ];
 
     console.log('Termina el controlador');
